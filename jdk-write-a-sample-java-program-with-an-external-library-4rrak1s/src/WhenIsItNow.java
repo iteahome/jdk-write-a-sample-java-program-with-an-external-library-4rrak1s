ed4dbd3b8f07178727c5949388fcabaecfa55bac
Flp <nuastazi@gmail.com>
2020-01-06 19:37:10 +0200

    forgot about the actual file

---
+++ jdk-write-a-sample-java-program-with-an-external-library-4rrak1s/src/WhenIsItNow.java
@@ -0,0 +1,9 @@
+import org.joda.time.*;
+
+    public class WhenIsItNow {
+        public static void main(String [] args) {
+
+            LocalTime localTime = new LocalTime();
+            System.out.println("Hello! It is " + localTime);
+        }
+    }
